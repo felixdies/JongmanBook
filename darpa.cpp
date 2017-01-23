72a3bcf9d56fedae61549f752592e4d82976b38c
felixdies <felixdies@daum.net>
2017-01-23 22:59:32 +0900

    darpa - skeleton

---
+++ darpa.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+/// 결정 문제: 정렬되어 있는 locations 중 cameras 를 선택해
+/// 모든 카메라 간의 간격이 gap 이상이 되는 방법이 있는지를 반환한다.
+bool decision(const vector<double>& location, int cameras, double gap) {
+}
+
+/// 최적화 문제: 정렬되어 있는 locations 중 cameras 를 선택해 최소 간격을 최대화한다.
+double optimize(const vector<double>& location, int cameras) {
+}
