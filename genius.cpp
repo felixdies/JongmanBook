ecb7d37ce85f77abdff99c30ad1b4b3520006066
felixdies <felixdies@daum.net>
2017-10-09 16:26:00 +0900

    답안 2. TLE
    - sliding window 의 index 계산 부분(5 modular 연산)이 복잡해보여서 변경.
    - 곱셈연산이 오래 걸려서 log 덧셈연산으로 바꾸면 될 지 궁금해서, log 변환 하지 않고 단순히 곱셈을 덧셈으로 변경 해 보았으나 여전히 시간초과ㅠㅠ

--- genius.cpp
+++ genius.cpp
@@ -9,12 +9,17 @@
 #include <set>
 #include <map>
 #include <bitset>
+#include <ctime>
+
+#define USE_TEXTFILE ifstream cin("jinput.txt"); ofstream cout("joutput.txt");
+#define USE_STOPWATCH std::clock_t start; double duration; start = std::clock();
+#define PRINT_TIME(start) cout << "\n[Time]" << (std::clock() - (start)) / (double)CLOCKS_PER_SEC << '\n';
 
 using namespace std;
 
 int main() {
-	//ifstream cin("jinput.txt");
-	//ofstream cout("joutput.txt");
+	//USE_TEXTFILE;
+	//USE_STOPWATCH; PRINT_TIME(start);
 
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
@@ -24,7 +29,7 @@ int main() {
 	int cs; cin >> cs;
 	while (cs--) {
 		int n, k, m; cin >> n >> k >> m;
-		
+
 		vector<int> musicLen(n);
 		for (int i = 0; i < n; i++) cin >> musicLen[i];
 
@@ -40,18 +45,25 @@ int main() {
 		dp[0][0] = 1;
 
 		int past = 0;
-		int cur;
+		int cur = 0;
 		while (k--) {
-			past = (past - 1) % 5;
-			cur = (5 + past) % 5;
+			// 0-4 까지의 시계를 시계 반대방향으로 한칸 돌린다고 생각하자
+			past++;
+			if (past == 5) past = cur = 0;
+			else cur = 5 - past;
+
 			for (int i = 0; i < n; i++) {
 				dp[cur][i] = 0;
 				for (int j = 0; j < n; j++) {
-					int prev = (5 + musicLen[j] + past) % 5;
-					dp[cur][i] += dp[prev][j] * mat[j][i];
+					// j번째 음악이 musicLen 전에 시작했을 확률은
+					// 시계방향으로 musicLen[j] 만큼 이동한 위치에 기록돼 있다
+					int prev = (cur + musicLen[j]) % 5; 
+					dp[cur][i] += dp[prev][j] + mat[j][i]; // 곱셈만 덧셈으로 바꾸어 봄-여전히 TLE
 				}
 			}
 		}
+
+		//PRINT_TIME(start); n=4, k=1,000,000 일 때 50초 이상 소요
 
 		for (int i = 0; i < m; i++) {
 			int idx = likes[i];
@@ -60,6 +72,7 @@ int main() {
 				ret += dp[(cur + j) % 5][idx];
 			cout << ret << ' ';
 		}
+		
 		cout << '\n';
 	}
 
